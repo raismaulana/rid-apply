e670aef74e830e8a239c90cefb6020413292812f
Lufri Rais Maulana <lufriraismaulana@gmail.com>
2021-06-06 11:18:49 +0700

    json manipulation 1-5

--- logic-test/logic-test.go
+++ logic-test/logic-test.go
@@ -3,14 +3,17 @@ package main
 import (
 	"time"
 
-	logic_test "github.com/raismaulana/rid-apply/logic-test"
+	logic_test "github.com/raismaulana/rid-apply/logic-test/jawaban"
 )
 
 func main() {
 	logic_test.Palindrome("Radar")
+	time.Sleep(1 * time.Second)
 	logic_test.Leap_year()
 	time.Sleep(1 * time.Second)
 	logic_test.Reverse()
+	time.Sleep(1 * time.Second)
 	logic_test.Nearest_fib([]int{15, 1, 3})
+	time.Sleep(1 * time.Second)
 	logic_test.FizzBuzz(15)
 }
